c74cc8d11fc4bb2741d55fb62e6a0652dfc4c002
Shaan <shaancitrom123@gmail.com>
2026-09-25 19:32:54 +0530

    Optimize production models for deployment

--- predict_match.py
+++ predict_match.py
@@ -2,7 +2,6 @@ import pandas as pd
 import joblib
 
 team_features = pd.read_csv("data/team_season_features.csv")
-
 home_model = joblib.load("models/home_goals_model_v3.pkl")
 away_model = joblib.load("models/away_goals_model_v3.pkl")
 feature_columns = joblib.load("models/model_features_v3.pkl")
